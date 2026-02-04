e3eb0ed018272e8d3026f7b1e686f7a63b26a426
lih8697-1 <lih8697@gmail.com>
2026-02-04 17:38:16 +0900

    3단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -1,8 +1,27 @@
 package com.back;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String cmd;
+        String wiseSaying;
+        String author;
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("== 명언 앱 ==");
-        System.out.println("명령) 종료");
+
+        while (true) {
+            System.out.printf("명령) ");
+            cmd = sc.next();
+
+            if (cmd.equals("등록")) {
+                System.out.printf("명언 : ");
+                wiseSaying = sc.next();
+                System.out.printf("작가 : ");
+                author = sc.next();
+                System.out.println("1번 명령이 등록되었습니다.");
+            }
+        }
     }
 }
